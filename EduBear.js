f204e458b897d66be0c44b6be2b164e6184ffeaf
IgnacySan <ignacysan27@gmail.com>
2023-03-06 22:52:04 +0100

    Change log() ------------------ - change unity language tutorial to c++
    - change of c++ language in the "future" tab to python
    
    - New thing "dropdown"
    
    - new img added: c++(1),c++(2),c++(3),c++(4),
     cplusplus-logo, python-icon and python-icon2
    -------------------

--- EduBear.js
+++ EduBear.js
@@ -3,3 +3,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
         hljs.highlightElement(el);
     });
 });
+function toggleDropdown(button) {
+    let dropdown = button.nextElementSibling;
+    dropdown.classList.toggle("show");
+
+    // zamknij wszystkie otwarte dropdowny
+    let dropdowns = document.querySelectorAll(".dropdown-content.show");
+    dropdowns.forEach(function(d) {
+        if (d !== dropdown) {
+            d.classList.remove("show");
+        }
+    });
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.drop-text-container')) {
+        let dropdowns = document.querySelectorAll(".dropdown-content.show");
+        dropdowns.forEach(function(d) {
+            d.classList.remove("show");
+        });
+    }
+}
+
